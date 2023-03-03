648e6baa340656af34f1971fa720bbc8f670de41
Victor Vlădulescu <victor.vladulescu@isidors.com>
2023-03-03 15:55:02 +0200

    M4-E1

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -15,10 +15,11 @@ import { Component, Input } from '@angular/core'
     </div>
   `,
   styles: [`
+    .thumbnail { min-height: 210px; }
     .pad-left { margin-left: 10px; }
     .well div { color: #bbb; }
   `]
 })
 export class EventThumbnailComponent {
   @Input() event:any
-} 
+}

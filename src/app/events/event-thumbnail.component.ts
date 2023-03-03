3cad2039f0a7b4a7d61133a383b975257752ec16
Victor Vlădulescu <victor.vladulescu@isidors.com>
2023-03-03 16:11:10 +0200

    M4-E2

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -9,8 +9,8 @@ import { Component, Input } from '@angular/core'
       <div>Time: {{event.time}}</div>
       <div>Price: \${{event.price}}</div>
       <div>
-        <span>Location: {{event.location.address}}</span>
-        <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+        <span>Location: {{event.location?.address}}</span>
+        <span class="pad-left">{{event.location?.city}}, {{event.location?.country}}</span>
       </div>
     </div>
   `,
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -21,11 +21,7 @@ export class EventsListComponent {
       time: '10:00 am',
       price: 599.99,
       imageUrl: '/assets/images/angularconnect-shield.png',
-      location: {
-        address: '1057 DT',
-        city: 'London',
-        country: 'England'
-      },
+      
       sessions: [
         {
           id: 1,
